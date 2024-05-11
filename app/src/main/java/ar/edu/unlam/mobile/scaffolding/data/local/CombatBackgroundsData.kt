9af496b85b80e7191112d61671094a6001091a77
GabiPezet <gabriel.pzt@gmail.com>
2024-05-11 00:27:06 -0300

    Commit adhesión de mapas de combate /revisión de resolución

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/local/CombatBackgroundsData.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/local/CombatBackgroundsData.kt
@@ -12,6 +12,13 @@ class CombatBackgroundsData @Inject constructor() {
         Background("Dragon Ball",R.drawable.iv_dragonballfight,R.raw.raw_dragonball),
         Background("Star Wars",R.drawable.iv_starwarsfight,R.raw.raw_starwars),
         Background("Street Fighters",R.drawable.iv_streetfighterfight,R.raw.raw_streetfighters),
-        Background("Mortal Kombat",R.drawable.iv_mortalkombatfight,R.raw.raw_mortalkombat)
+        Background("Mortal Kombat",R.drawable.iv_mortalkombatfight,R.raw.raw_mortalkombat),
+        Background("Titan Planet", R.drawable.iv_titan_planet, R.raw.raw_arenafight),
+        Background("Cyberpunk", R.drawable.iv_cyberpunk_escenario, R.raw.raw_arenafight),
+        Background("Dune", R.drawable.iv_dune, R.raw.raw_arenafight),
+        Background("Sky Platform", R.drawable.iv_escenario_plataforma_cielo, R.raw.raw_arenafight),
+        Background("Simple Ring", R.drawable.iv_ring_sencillo, R.raw.raw_arenafight),
+        Background("Terror Scene", R.drawable.iv_terror_scene, R.raw.raw_arenafight),
+        Background("Ribbon Ring", R.drawable.iv_ribbon_ring, R.raw.raw_arenafight)
     )
 }
