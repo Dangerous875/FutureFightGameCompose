49f2169c27f3f8f86770c485873dfd3655532cf2
Dangerous875 <crbarzabal@gmail.com>
2024-06-10 20:03:08 -0300

    fix color status bar

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/theme/Theme.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/theme/Theme.kt
@@ -7,10 +7,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -23,11 +21,11 @@ private val DarkColorScheme =
         tertiary = Pink80,
     )
 
-private val LightColorScheme =
-    lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40,
+//private val LightColorScheme =
+//    lightColorScheme(
+//        primary = Purple40,
+//        secondary = PurpleGrey40,
+//        tertiary = Pink40,
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -37,7 +35,7 @@ private val LightColorScheme =
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
      */
-    )
+//    )
 
 @Composable
 fun ScaffoldingV2Theme(
