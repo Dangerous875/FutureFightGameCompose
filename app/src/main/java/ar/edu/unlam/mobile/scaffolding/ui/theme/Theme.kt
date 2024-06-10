71fc99e9dee632246e8750d6beb3281562e44feb
Dangerous875 <crbarzabal@gmail.com>
2024-06-10 20:02:06 -0300

    fix color status bar

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/theme/Theme.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -60,7 +61,7 @@ fun ScaffoldingV2Theme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.background.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
     }
 
