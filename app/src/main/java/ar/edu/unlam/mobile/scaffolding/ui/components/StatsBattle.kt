aade2aa5d6b7b875497f6209d32996269f1d8475
Dangerous875 <crbarzabal@gmail.com>
2024-06-17 15:02:32 -0300

    FIx COMBAT SCREEN

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/StatsBattle.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/StatsBattle.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
